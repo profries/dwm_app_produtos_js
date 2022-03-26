91d4e0386a2c8d4b8d64e6f9ac964c355df16028
Luis Henrique Ries <prof.ries@gmail.com>
2022-03-26 14:22:42 -0300

    App final - renderiznado os produtos

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-window.onload = function () {
+window.onload = function () {    
+
     buscarProdutos();
     setInterval(buscarProdutos, 10000);
 }
@@ -6,23 +7,53 @@ window.onload = function () {
 function buscarProdutos() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        document.querySelector("#produtos").innerHTML =
-        this.responseText;
+        //document.querySelector("#produtos").innerHTML =        
+        const prod = JSON.parse(this.responseText);
+        carregarProdutos(prod);
     }
     xhttp.open("GET", "http://localhost:3000/produtos");
     xhttp.send();
 }
 
-function carregarProdutos() {
-    console.log("Carregando produtos");
-    document.querySelector("#produtos").innerHTML = `
-    <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
-        <div class="w3-card">
-            <div class="w3-container w3-center">
-                <img src="pedigree.jpg" style="width: 70%">
-                <h5>Informações do Produto - lorem ipsum dolor</h5>
-                <h3 class="w3-blue">R$ 100,00</h3>
+function carregarProdutos(produtos) {
+    let htmlProdutos = (produtos.length == 0) 
+        ? "<h1>Não há produtos cadastrados</h1>"
+        : produtos.map (prod => 
+            `
+            <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+                <div class="w3-card">
+                    <div class="w3-container w3-center">
+                        <img src="${prod.imagem}" style="width: 70%">
+                        <h5>${prod.nome}</h5>
+                        <h3 class="w3-blue">
+                            ${'R$ '+prod.preco.toLocaleString(
+                                'pt-br', {minimumFractionDigits: 2})
+                            }
+                        </h3>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>`
+            `).join(' ');
+
+   /* produtos.forEach( (prod) =>{ 
+    //for(let prod of produtos){
+        htmlProdutos +=     
+            `
+            <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+                <div class="w3-card">
+                    <div class="w3-container w3-center">
+                        <img src="${prod.imagem}" style="width: 70%">
+                        <h5>${prod.nome}</h5>
+                        <h3 class="w3-blue">
+                            ${'R$ '+prod.preco.toLocaleString(
+                                'pt-br', {minimumFractionDigits: 2})
+                            }
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            `;
+    })*/
+
+    document.querySelector("#produtos").innerHTML = htmlProdutos;
 }

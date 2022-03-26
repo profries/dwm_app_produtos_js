3cc7032e134039f0feefa6e27f273de7d49cb084
Luis Henrique Ries <prof.ries@gmail.com>
2022-03-26 11:10:23 -0300

    Adicionando timing na aplicacao para carregar os produtos

--- public/app.js
+++ public/app.js
@@ -1,12 +1,18 @@
-//document.getElementById("produtos").innerHTML = "<h1>Produtos</h1>";
-document.querySelector("#produtos").innerHTML = `
-<div class="w3-col l4 m6 s12 w3-container w3-padding-16">
-    <div class="w3-card">
-        <div class="w3-container w3-center">
-            <img src="pedigree.jpg" style="width: 70%">
-            <h5>Informações do Produto - lorem ipsum dolor</h5>
-            <h3 class="w3-blue">R$ 100,00</h3>
+window.onload = function () {
+    carregaProdutos();
+    setInterval(carregaProdutos, 10000);
+}
+
+function carregaProdutos() {
+    console.log("Carregando produtos");
+    document.querySelector("#produtos").innerHTML = `
+    <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+        <div class="w3-card">
+            <div class="w3-container w3-center">
+                <img src="pedigree.jpg" style="width: 70%">
+                <h5>Informações do Produto - lorem ipsum dolor</h5>
+                <h3 class="w3-blue">R$ 100,00</h3>
+            </div>
         </div>
-    </div>
-</div>
-`;
+    </div>`
+}

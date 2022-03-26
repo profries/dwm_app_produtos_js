7fe91ed20c7c16ebfa13f56676a8681a617669ea
Luis Henrique Ries <prof.ries@gmail.com>
2022-03-26 10:38:05 -0300

    Template da aplicacao e parte1 da aplicacao JS

---
+++ public/w3css.js
@@ -0,0 +1,37 @@
+//Tratamento do menu
+function mostrarOpcoes() {
+    var x = document.getElementById("menu-mobile");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+//Tratamento do slideshow (carroussel)
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " w3-white";
+}
